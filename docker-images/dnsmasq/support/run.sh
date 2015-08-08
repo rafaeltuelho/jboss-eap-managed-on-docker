f3e2033300d9579811d52797c0ddacfdee41c723
rafaeltuelho <rafaelcba@gmail.com>
2015-08-07 21:45:37 -0300

    fix dnsmasq stop trait

--- docker-images/dnsmasq/support/run.sh
+++ docker-images/dnsmasq/support/run.sh
@@ -2,6 +2,17 @@
 
 echo -e "\n\t >>> starting dnsmaq service for local Docker containers <<< \n"
 
+stop_container(){
+        echo -e "\n\t >>> shutdown the container process...\n"
+        /usr/sbin/incrond -k
+        pkill dnsmasq
+
+        echo "exited $0"
+}
+
+# catch the stop/kill signals from shell
+trap 'echo TRAPed signal; stop_container' HUP INT QUIT KILL TERM
+
 # start services daemons
 /usr/sbin/incrond
 /usr/sbin/dnsmasq -d
